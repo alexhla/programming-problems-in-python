c0512b4f1435b0f8886742a72ced72a1ee4b0991
alexhla <astutealex@gmail.com>
2018-09-26 12:54:41 -0700

    added 2nd solution to Reverse Integer

--- reverse_integer.py
+++ reverse_integer.py
@@ -15,7 +15,17 @@ class Solution:
 		else:
 			return sign * n 	# recombine with sign and return
 
+	def reverse2(self, x):
+		sign = -1 if x<0 else 1
+		s = str(abs(x))			# convert absolute value of int to string to avoid dash
+		n = sign*int(s[::-1])	# reverse string and convert back to int adding sign last
+		if (n > (2**31)-1) or (n < -2**31):  # check for overflow
+			return 0
+		else:
+			return n
+
 obj = Solution()
 num = 123456789
 print("Reversing {}" .format(num))
 print("Answer-1: {}" .format(obj.reverse(num)))
+print("Answer-2: {}" .format(obj.reverse2(num)))
