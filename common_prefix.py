1ed1ea9717670fea9cc0d5739219de4d252167fd
alexhla <astutealex@gmail.com>
2018-09-27 14:27:20 -0700

    commented Longest Common Prefix

--- common_prefix.py
+++ common_prefix.py
@@ -4,13 +4,13 @@ class Solution:
 			return ""
 
 		ans = ""
-		for i, ch in enumerate(strs[0]):
-			for j, s in enumerate(strs):
-				if i >= len(strs[j]) or ch != s[i]:
-					return ans
-			ans += ch
+		for i, ch in enumerate(strs[0]):  # iterate over the characters of first word
+			for j, s in enumerate(strs):  # iterate over the list of words
+				if i >= len(strs[j]) or ch != s[i]:  # IF the first word is longer
+					return ans  # OR the characters do not match return answer string
+			ans += ch  # ELSE add the current character to answer
 
-		return ans
+		return ans  # all words are identical or longer than first
 
 obj = Solution()
 l = ["flower","flow","flight", "flipped"]
