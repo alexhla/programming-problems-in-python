347ba9917d39f450b683b24b687c8f6ec0d49950
alexhla <astutealex@gmail.com>
2018-09-30 23:43:19 -0700

    added comments to Isomorphic Strings

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,7 +1,7 @@
 class Solution:
 	def isIsomorphic(self, s, t):
 
-		if len(s) != len(t):
+		if len(s) != len(t):  # isomorphic strings must be same length
 			return False
 
 		d = {}
@@ -12,19 +12,22 @@ class Solution:
 			x = s[i]
 			y = t[i]
 
-			if x not in d and y not in h:
-				d[x] = [i]
-				h[y] = [i]
+			if x not in d and y not in h:  # Both chars have NOT been seen before (NOT in hash table)
+				d[x] = [i]  # store index as a single element list since
+				h[y] = [i]  # repeat character indices must be appended
 
-			elif x in d and y in h:
-				d[x].append(i)
+			elif x in d and y in h:  # both chars seen before (EXIST in hash table)
+				d[x].append(i)    # append new index to list
 				h[y].append(i)
-				if d[x] != h[y]:
-					return False
-			else:
+				if d[x] != h[y]:  # ensure chars have been seen at the same indices
+					return False  # if indices do NOT match up strings are not isomorphic
+
+			# One char has been seen before but NOT the other
+			# if (x not in d and y in h) or (x in d and y not in h):
+			else:  # only four possible outcomes thus else is sufficient
 				return False
 
-		return True
+		return True  # strings are isomorphic return true
 
 obj = Solution()
 s1 = "paper"
