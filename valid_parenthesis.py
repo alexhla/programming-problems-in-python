cffde389e98c40e0926dcffc6fd84933768d1496
alexhla <astutealex@gmail.com>
2018-09-30 23:48:22 -0700

    commented Valid Parenthesis

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,20 +1,20 @@
 class Solution:
 	def isValid(self, s):
-		if not s:
+		if not s:		# empty string is valid
 			return True
 
 		stack = []
 		for ch in s:
-			if ch == '(' or ch == '[' or ch == '{':
+			if ch == '(' or ch == '[' or ch == '{':  # push opening brackets onto stack
 				stack.append(ch)
-			elif not stack:
-				return False
+			elif not stack:  # if stack is empty then cannot pop needed closing bracket
+				return False # string is invalid
 			else:
-				x = stack.pop()
+				x = stack.pop()  # else pop and check if the closing bracket matches
 				if ch == ')' and x != '(' or ch == ']' and x != '[' or ch == '}' and x != '{':
 					return False
 
-		return True if not stack else False
+		return True if not stack else False # string valid only if stack is empty
 
 obj = Solution()
 s = "([[]])()()()"
