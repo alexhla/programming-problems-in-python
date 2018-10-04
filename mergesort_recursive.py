12be4cf9d4dc4a4d6ba9f1bb814861ff9573b9ba
alexhla <astutealex@gmail.com>
2018-10-04 15:22:30 -0700

    added comments to Mergesort Recursive

--- mergesort_recursive.py
+++ mergesort_recursive.py
@@ -1,18 +1,28 @@
+# Mergesort is the best sorting algorithm for use with a linked-list
+
+# Time  Complexity  O(nlogn)
+
+	# Merging will require log(n) doublings from subarrays of size (1) to a single array of size length(n), 
+	# where each pass will require (n) iterations to compare and sort each element
+
+# Space Complexity  O(n)  arrays
+#					O(1)  linked-list
+
 def mergesort(a):
-	if len(a) > 1:  # divide before conquering
-		m = len(a)//2  # floor division to avoid floats
+	if len(a) > 1:  # divide array into subarrays of length one
+		m = len(a)//2  # floor division returns a truncated integer by rounding towards negative infinity
 		l = a[:m]
 		r = a[m:]
 
-		mergesort(l)
+		mergesort(l)  # create auxillary arrays requiring O(n) memory in addition to the call stack overhead
 		mergesort(r)
 
-		i=0
-		j=0
-		k=0
+		i=0  # left half index
+		j=0  # right half index
+		k=0  # merge array index
 
-		while i < len(l) and j < len(r):  # compares occur nlog(n) times
-			if l[i] < r[j]:               # each time you divide 
+		while i < len(l) and j < len(r):  # WHILE left and right halves both contain elements
+			if l[i] < r[j]:
 				a[k]=l[i]
 				i += 1
 			else:
@@ -20,12 +30,12 @@ def mergesort(a):
 				j += 1
 			k += 1
 
-		while i < len(l):
+		while i < len(l):  # WHILE only left half contains elements
 			a[k]=l[i]
 			i += 1
 			k += 1
 
-		while j < len(r):
+		while j < len(r):  # WHILE only right half contains elements
 			a[k]=r[j]
 			j += 1
 			k += 1
