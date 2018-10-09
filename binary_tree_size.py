3e11836c3e8199468ae3558dc3b3bb912f491d62
alexhla <astutealex@gmail.com>
2018-10-08 20:11:37 -0700

    added a more compact implementation

--- binary_tree_size.py
+++ binary_tree_size.py
@@ -10,16 +10,17 @@ class Solution:
 		if A == None:
 			return 0
 
-		count = 1
-		count += self.countnodes(A.left)
-		count += self.countnodes(A.right)
+		# count = 1
+		# count += self.countnodes(A.left)
+		# count += self.countnodes(A.right)
+		# return count
 
-		return count
+		# A More Compact Implementation
 
+		return 1 + self.countnodes(A.left) + self.countnodes(A.right)
 
 
 obj = Solution()
-
 root = Node(4)
 root.left = Node(2)
 root.right = Node(5)
@@ -27,6 +28,4 @@ root.left.left = Node(1)
 root.left.right = Node(3)
 root.right.left = Node(1)
 root.right.right = Node(3)
-
-
 print("Number of Tree Nodes is {}" .format(obj.countnodes(root)))
