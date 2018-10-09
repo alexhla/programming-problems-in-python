1ec1c5b2e806cf252942c9d3a43945f7716caf24
alexhla <astutealex@gmail.com>
2018-10-08 19:56:27 -0700

    added Binary Tree Size

---
+++ binary_tree_size.py
@@ -0,0 +1,32 @@
+class Node:
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution:
+	def countnodes(self, A):
+		
+		if A == None:
+			return 0
+
+		count = 1
+		count += self.countnodes(A.left)
+		count += self.countnodes(A.right)
+
+		return count
+
+
+
+obj = Solution()
+
+root = Node(4)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(1)
+root.left.right = Node(3)
+root.right.left = Node(1)
+root.right.right = Node(3)
+
+
+print("Number of Tree Nodes is {}" .format(obj.countnodes(root)))
