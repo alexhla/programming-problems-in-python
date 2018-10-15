09304d58e138c5f0b4bd2c8f0e29862fa20ba3ab
alexhla <astutealex@gmail.com>
2018-10-15 14:41:21 -0700

    commented Palindrome Integer

--- palindrome_integer.py
+++ palindrome_integer.py
@@ -1,21 +1,21 @@
 class Solution:
 	def isPalindrome(self, x):
-		if x < 0:
-			return False
+		if x < 0:			# negative integers
+			return False	# are not palindromes
 
 		rnum = 0;
 		temp = x;
 		while temp != 0:
-			rnum *= 10
-			rnum += temp%10;
-			temp //= 10;
+			rnum *= 10		  # left shift digits one place 
+			rnum += temp%10;  # push least significant digit
+			temp //= 10;	  # pop processed digit
 
-		return True if x == rnum else False
+		return True if x == rnum else False  # compare integers and return
 
 	def isPalindrome2(self, x):
-		s = str(x)
-		rs = s[::-1]
-		return True if s == rs else False
+		s = str(x)		# convert integer to string
+		rs = s[::-1]	# reverse string
+		return True if s == rs else False  # compare strings and return
 
 
 obj = Solution()
